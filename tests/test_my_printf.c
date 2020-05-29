72f52b31e0c425c48970fd71d5ff74a4cb48fc9a
Clement Madzar <cmadzar@localhost.localdomain>
2020-05-29 12:21:13 +0200

    .

--- tests/test_my_printf.c
+++ tests/test_my_printf.c
@@ -15,9 +15,11 @@ Test(my_printf, display_int)
     char str[] = "Hello";
     char str_b[] = "Ha";
 
-    cr_expect_not(my_printf("j'ai %d e%c ans\n", str, t), "j'ai 1234 et ans\n");
+    cr_expect_not(my_printf("j'ai %d e%c ans", i, t), "j'ai 1234 et ans");
     cr_expect_not(my_printf("%s toi !"), "Hello toi !");
-    cr_expect_not(my_printf("%u / %S / %b / %o", un_i, str, str_b, un_i), "99 / HELLO / 01001000 01100001 / 143");
-    cr_expect_not(my_printf("%x / %X / %p %%", i, i), "4d2 / 4D2 / 0x4df6bb2a %");
-    cr_expect_not(my_printf("A+% C % B0% G#%-%"), "A+C B0G#0x");
+    cr_expect_not(my_printf("%u / %S / %b / %o", un_i, str, str_b, un_i),
+        "99 / HELLO / 01001000 01100001 / 143");
+    cr_expect_not(my_printf("%x / %X / %p %%", i, i),
+        "4d2 / 4D2 / 0x4df6bb2a %");
+    cr_expect_not(my_printf("A+% C % B0%+ G#%-%0"), "A+C B 0G#0x");
 }
